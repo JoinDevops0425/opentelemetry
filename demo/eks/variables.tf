52edb675cbd661a82e3a44c5aec7f067837cde48
Uday <JoinDevops0425@gmail.com>
2026-04-13 14:58:02 +0530

    Create variables.tf

---
+++ demo/eks/variables.tf
@@ -0,0 +1,29 @@
+variable "cluster_name" {
+  description = "I am roles for cluster"
+  type = string
+}
+
+variable "subnet_ids" {
+  description = "subnet Id"
+  type = list(string)
+}
+
+variable "node_groups" {
+  description = "node group name"
+  type = list(string)
+}
+
+
+variable "node_groups" {
+  description = "EKS node group configuratoin"
+  type = map(object({
+    instance_type = list(string)
+    capacity_type = string
+    scaling_config = object({
+      desired_size = number
+      max_size = number
+      min_size = number 
+    })
+    
+  }))
+}
